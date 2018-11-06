68273859a6b46c2b1bfc49fcac4658740f08906c
Anthony Moreno <tonymoreno86@gmail.com>
2018-11-05 19:28:25 -0800

    Updated schema by removing duplicate fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106003841) do
+ActiveRecord::Schema.define(version: 20181105211502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20181106003841) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.text "main_image"
   end
 
   create_table "reviews", force: :cascade do |t|
